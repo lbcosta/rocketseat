993789d143e4a0677ae61084aaab75f8a224ec8d
Leonardo Costa <leocosta@edu.unifor.br>
2019-11-13 09:07:53 -0300

    M04A15 e A16

---
+++ api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '11be09e283cb37',
+    pass: '66510ba36b3c0f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

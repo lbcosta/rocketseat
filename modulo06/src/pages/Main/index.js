dd57176b7810563e54e5f6a4381b4aeffe216518
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-16 19:03:36 -0300

    Starting Main page

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -1,8 +1,76 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default function Main() {
-  return <View />;
+  const [newUser, setNewUser] = useState('');
+  const [users, setUsers] = useState([]);
+
+  async function handleAddUser() {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  }
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={setNewUser}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
+        />
+        <SubmitButton onPress={handleAddUser}>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Ver perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
+    </Container>
+  );
 }
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};

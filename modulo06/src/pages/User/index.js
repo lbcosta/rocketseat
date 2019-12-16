326db3a3388e55123850eea804cef696428cdb91
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-16 20:13:11 -0300

    Finished Module 06

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,9 +1,67 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+  const [stars, setStars] = useState([]);
+  const user = navigation.getParam('user');
+
+  useEffect(() => {
+    async function getData() {
+      const user = navigation.getParam('user');
+
+      const response = await api.get(`users/${user.login}/starred`);
+      setStars(response.data);
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={star => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
+  );
 }
+
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};

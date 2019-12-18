2a55a55b6042e4b85da7a7c55d0c860b911ab592
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-18 15:09:22 -0300

    REMOVE_FROM_CART action

--- modulo07/src/pages/Cart/index.js
+++ modulo07/src/pages/Cart/index.js
@@ -7,7 +7,7 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -24,7 +24,7 @@ function Cart({ cart }) {
           {cart.map(product => (
             <tr>
               <td>
-                <img src={product.img} alt={product.title} />
+                <img src={product.image} alt={product.title} />
               </td>
               <td>
                 <strong>{product.title}</strong>
@@ -45,7 +45,12 @@ function Cart({ cart }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>

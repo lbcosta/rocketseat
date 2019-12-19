08b8b8dbc1ef3d3867ff0a9f544ec66169488233
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-18 21:35:00 -0300

    React Toastify

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
 
@@ -14,9 +15,10 @@ function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <GlobalStyle />
         <Header />
         <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );

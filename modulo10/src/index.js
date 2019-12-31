f45eea364ade4c616e53cb054409d455b7bd5c4e
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-31 17:04:55 -0300

    Deleting boilerplate

--- modulo10/src/index.js
+++ modulo10/src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your a!</Text>
-    </View>
-  );
+  return <View />;
 }

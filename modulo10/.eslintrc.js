cafb48b29e0512010c11cc2016bfacd0056a576d
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-31 19:10:07 -0300

    Configurando Root Import

--- modulo10/.eslintrc.js
+++ modulo10/.eslintrc.js
@@ -22,4 +22,11 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
--- modulo10/src/index.js
+++ modulo10/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { View } from 'react-native';
+
+import Teste from '~/Teste';
 
 import './config/ReactotronConfig';
 
 export default function App() {
-  return <View />;
+  return <Teste />;
 }
